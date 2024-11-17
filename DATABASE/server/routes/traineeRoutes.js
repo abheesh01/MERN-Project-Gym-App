4587bf86f6dfe626a2c3516c9ea0c7ae45e23a8c
rahulg221 <rdg.ghosh@gmail.com>
2024-11-17 13:56:38 -0500

    changed structure, used react vite stuff

--- DATABASE/server/routes/traineeRoutes.js
+++
@@ -1,9 +0,0 @@
-// server/routes/traineeRoutes.js
-const express = require('express');
-const router = express.Router();
-const traineeController = require('../controllers/traineeController');
-
-router.post('/add', traineeController.addTrainee);
-router.get('/', traineeController.getTrainees);
-
-module.exports = router;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink, Navigate  } from 'react-router-dom';
+import RegisterPage from './components/RegisterPage.tsx';
+import SignInPage from './components/SignInPage.tsx';
+import DashboardPage from './components/DashboardPage.tsx';
+
+const App: React.FC = () => {
+  const [signedIn, setSignedIn] = React.useState(false);
+
+  const handleSignIn = () => setSignedIn(true);
+  const handleSignOut = () => setSignedIn(false);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInPage onSignIn={handleSignIn} />} />  {/* Set SignInPage as the default page */}
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route 
+          path="/dashboard"
+          element={signedIn ? (
+            <DashboardPage
+              name="John Doe"
+              gym="UCF RWC"
+              workoutType="Bulk"
+              timings="Afternoon (12PM-5PM)"
+              idealRate="Basic ($10-$20)"
+              userType='trainee'
+              onSignOut={handleSignOut}
+            />
+          ) : (
+            <Navigate to="/" replace />
+          )}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
