93b6cb5e48d06f0a4f04d2eb469512ec955a69ab
Apatel1103 <148724655+Apatel1103@users.noreply.github.com>
2024-11-21 17:34:46 -0500

    Update userRoutes.js with login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { registerUser } = require('../controllers/userController');
+const { loginUser } = require('../controllers/userController')
 const router = express.Router();
 
 // Route to register a new user
 router.post('/register', registerUser);
+
+// Route to login a new user
+router.post('/login', loginUser)
 
 module.exports = router;
